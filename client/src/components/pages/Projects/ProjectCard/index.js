3c67c300a74253d6e69f1039fb8d2bb4ca10f329
nelson1812 <nelsoncc1812@gmail.com>
2020-06-25 14:43:39 +0100

    tested and fixed projects database thinks q q echo Hello hello hello

--- client/src/components/pages/Projects/ProjectCard/index.js
+++ client/src/components/pages/Projects/ProjectCard/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export default function ProjectCard_({ order, name, description, objetive, duration, image, link }) {
+
+    console.log(image)
+
     return (
-        <h4>{ name }</h4>
+        <div style={ { border: '1px solid red' } }>
+            <h3>Order: { order }</h3>
+            <h3>Name: { name }</h3>
+            <h3>Descrition: { description }</h3>
+            <h3>Objetive: { objetive }</h3>
+            <h3>Duration: { duration }</h3>
+            <h3>Image</h3><img src={ image } />
+            <a href={ link }>Link </a>
+        </div>
+
     )
 }
