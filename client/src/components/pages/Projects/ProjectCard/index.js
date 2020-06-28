1c78705ab3dad0a0932cc9dfbded76f9256fd0b8
nelson1812 <nelsoncc1812@gmail.com>
2020-06-28 16:05:58 +0100

    some changes done

--- client/src/components/pages/Projects/ProjectCard/index.js
+++ client/src/components/pages/Projects/ProjectCard/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
+import './projectCard.css'
+
 export default function ProjectCard_({ order, name, description, objetive, duration, image, link }) {
 
-    console.log(image)
-
     return (
-        <div style={ { border: '1px solid red' } }>
+        <div className='projectCard'>
             <h2> { name }</h2>
-            <img src={ image } />
-            <h3>Order: { order }</h3>
-            <h3>Descrition: { description }</h3>
-            <h3>Objetive: { objetive }</h3>
-            <h3>Duration: { duration }</h3>
-            <a rel='noopener noreferrer' target='_blank' href={ link }>Link to web { order > 1 && '(delays first time because free CDN politics)' } </a>
+            <div style={ { paddingTop: '40px', borderTop: '2px solid black', borderBottom: '2px solid black' } }>
+                <img src={ image } />
+                <h3>Order: { order } </h3>
+                <h3>Descrition: { description }</h3>
+                <h3>Objetive: { objetive }</h3>
+                <h3>Duration: { duration } weeks</h3>
+                <a style={ { color: 'rgb(130,130,250)' } } rel='noopener noreferrer' target='_blank' href={ link }>Link to web { order > 1 && '(delays first time because free CDN politics)' } </a>
+            </div>
         </div>
 
     )
