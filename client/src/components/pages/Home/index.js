3c222989fee23cff4b59660fea45476ee51849be
nelson1812 <nelsoncc1812@gmail.com>
2020-06-30 23:39:27 +0100

    projects done

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -11,7 +11,7 @@ export default function () {
         <div className='pagesContainer'>
             <h1 style={ { marginBottom: '30px' } }>Home</h1>
 
-            <p >Thats a provisional portfolio, realized in less than a week to serve a provisional porpouse. I am actually learning about Redux. Then I will create a definitive porfolio, the code of this portfolio (maybe esthetically is not too visual) is in this link <a href='https://github.com/nelsoncc1812/provisional-portfolio'> link</a>. This portfolio is realized in React, using too some interesting technologies as: Boostrap, axios... The messages system its currently working online, so I can see messages if you want to contact with me.
+            <p >Thats a provisional portfolio, realized in less than a week to serve a provisional porpouse. I am actually learning about Redux. Then I will create a definitive porfolio, the code of this portfolio (maybe esthetically is not too visual) is in this <a style={ { color: 'rgb(130,130,250)' } } href='https://github.com/nelsoncc1812/provisional-portfolio'> link</a>. This portfolio is realized in React, using too some interesting technologies as: Boostrap, axios... The messages system its currently working online, so I can see messages if you want to contact with me.
             </p>
             <img style={ { width: '80%' } } src='./images/home/rage.gif' alt='working while raging' />
 
--- client/src/components/pages/Projects/index.js
+++ client/src/components/pages/Projects/index.js
@@ -13,9 +13,17 @@ import './projects.css'
 
 export default function Projects_() {
     return (
-        <div className='pagesContainer'>
-            <h1>Projects</h1>
-            { projectsDB.map(elm => <ProjectCard key={ elm.order } { ...elm } />) }
-        </div>
+        <>
+            <div className='pagesContainer'>
+                <h1>Projects</h1>
+                { projectsDB.map(elm => <ProjectCard key={ elm.order } { ...elm } />) }
+            </div>
+
+            <div style={ { backgroundColor: 'rgb(240, 240, 235)', paddingTop: '50px', paddingBottom: '30px', width: '100%', padding: '50px 80px 30px 80px' } }>
+                <h2>Actually</h2>
+                <hr />
+                <p>I am currently in active work search, studying a Redux course (when I finish I will create a better version of a portfolio). Then I want to study a course of Docker, Kubernetes and Jenkins (separated)</p>
+            </div>
+        </>
     )
 }
