abaf5b5d3930cacd3847fcb0dbc21a8686d6b3c3
nelson1812 <nelsoncc1812@gmail.com>
2020-07-01 11:17:20 +0100

    Deploy

--- README.md
+++ README.md
@@ -17,8 +17,11 @@
 
 ## Deploy (from /)
 ````
-rm -rf client/build; rm -rf server/public/*; cd client; npm run build-dev; cd ..; mv client/build/* server/public; rm -rf client/build
+rm -rf client/build; rm -rf server/public/*; cd client; npm run build-prod; cd ..; mv client/build/* server/public; rm -rf client/build
 ````
+```
+git add . ; git commit -m "Deploy" ; git push heroku master
+```
 
 
 # Pasos a seguir
---
+++ server/public/precache-manifest.a8aa2dbb8e6aed76ec0fc991fdc1bf40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d6d993d41555de57304d38298e9a25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59fdc5c2cf568413138c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f2794a1fda1db5f89e81",
+    "url": "/static/css/main.570dfd40.chunk.css"
+  },
+  {
+    "revision": "59fdc5c2cf568413138c",
+    "url": "/static/js/2.d8ac754d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d8ac754d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2794a1fda1db5f89e81",
+    "url": "/static/js/main.48f18342.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
