13cd97503d97ebe5ad96158ea61e29b5863a427e
nelson1812 <nelsoncc1812@gmail.com>
2020-07-18 21:10:12 +0100

    Deploy

---
+++ server/public/precache-manifest.00c374348a14ed6d3dfdb2bf7b443177.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3714006d270a766b02d378c49d36fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c3f16f6e96451caee5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "af274ce5e39708ec4285",
+    "url": "/static/css/main.7dea5980.chunk.css"
+  },
+  {
+    "revision": "27c3f16f6e96451caee5",
+    "url": "/static/js/2.30de95f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.30de95f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af274ce5e39708ec4285",
+    "url": "/static/js/main.07f1c2ad.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85b627b27bc4bf2cb286fd08b092a476.js"
+  "/precache-manifest.00c374348a14ed6d3dfdb2bf7b443177.js"
 );
 
 self.addEventListener('message', (event) => {
